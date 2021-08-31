1ad8dc41865c6302835ee432eeb617140b525419
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 19:06:42 -0400

    part 17

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,18 @@
   <div class='dashboard'>
     <h1 class='subheading grey--text'>Dashboard</h1>
     <v-container class='my-5'>
+  
+      <v-layout row class='mb-3'>
+        <v-btn small flat color='grey' @click='sortBy("title")'>
+          <v-icon left small>folder</v-icon>
+          <span class='caption text-lowercase'>By project name</span>
+        </v-btn>
+        <v-btn small flat color='grey' @click='sortBy("person")'>
+          <v-icon left small>person</v-icon>
+          <span class='caption text-lowercase'>By person</span>
+        </v-btn>
+      </v-layout>
+
       <v-card flat class='pa-3' v-for='project in projects' :key='project.title'>
         <v-layout row :class='`pa-3 project ${project.status}`'>
           <v-flex xs12 md6>
@@ -17,16 +29,15 @@
             <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class='right'>
-              <v-chip small :class='`${project.status} white--text caption my-2`'>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text caption my-2`">
                 {{ project.status }}
               </v-chip>
             </div>
           </v-flex>
         </v-layout>
-        
-      </v-card>
-    <v-divider></v-divider>
+        <v-divider></v-divider>    
+      </v-card>  
     </v-container>
   </div>
 </template>
@@ -42,11 +53,16 @@ export default {
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
     }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .project.complete {
   border-left: 4px solid #3cd1c2;
 }
@@ -59,15 +75,15 @@ export default {
   border-left: 4px solid #f83870;
 }
 
-.v-chip.complete {
-  background-color: #3cd1c2;
+.v-chip.complete:not(.v-chip--active) {
+  background: #3cd1c2;
 }
 
-.v-chip.ongoing {
-  background-color: #ffaa2c;
+.v-chip.ongoing:not(.v-chip--active) {
+  background: #ffaa2c;
 }
 
-.v-chip.overdue {
-  background-color: #f83870;
+.v-chip.overdue:not(.v-chip--active) {
+  background: #f83e70;
 }
 </style>
