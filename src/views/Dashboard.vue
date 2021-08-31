294972991e905bf5f28c293b2188fbd1a9a0b695
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 15:48:19 -0400

    part 14

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,29 +2,26 @@
   <div class='dashboard'>
     <h1 class='subheading grey--text'>Dashboard</h1>
     <v-container class='my-5'>
-      <v-layout row>
-        <v-flex xs12 md6>
-          <v-btn outline block class='primary'>1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class='primary'>2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class='primary'>3</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class='primary'>4</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-space-around>
-        <v-flex xs4 md3>
-          <v-btn outline block class='success'>1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class='success'>2</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card flat class='pa-3'>
+        <v-layout row>
+          <v-flex xs12 md6>
+            <div class='caption grey--text'>Project Title</div>
+            <div>Create a website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class='caption grey--text'>Person</div>
+            <div>Net Ninja</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class='caption grey--text'>Due by</div>
+            <div>Jan 1st</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class='caption grey--text'>Status</div>
+            <div>Ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
