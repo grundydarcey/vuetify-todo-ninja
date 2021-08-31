23d6a0ca041bf7aa7371e48fc809cb28b1ab7214
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 13:53:31 -0400

    part 5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
   <div class='home'>
     <h1>Home page</h1>
-    <p class='red white--text'>lorem  ipsum</p>
-    <p class='pink lighten-4 red--text text--darken-4'>loreino ipsumo</p>
-    <h1 class='display-4'>Massive Display</h1>
-    <h4 class='display-1'>Small dis</h4>
-    <p class='headline'>Headline</p>
-    <p class='subheading font-weight-bold'>Subheading here</p>
-    <p class='caption'>Caption here</p>
+    <v-btn class='pink white--text'>Click Me</v-btn>
+    <v-btn depressed color='pink' >Click mee</v-btn>
+    <v-btn flat color='pink'>Click</v-btn>
+    <v-btn depressed class='pink white--text'>
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+    <v-btn depressed small class='pink white--text'>
+      <v-icon small left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+    <v-btn large depressed class='pink white--text'>
+      <v-icon small right>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+    <v-btn fab depressed small dark color='purple'>
+      <v-icon>favorite</v-icon>
+    </v-btn>
   </div>
 </template>
 
