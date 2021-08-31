f262a4f97c8a90094bb875a298835fff35afe4f7
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 14:27:32 -0400

    part 8

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,21 +1,29 @@
 <template>
   <nav>
-    <v-toolbar flat app>
+    <v-app-bar app>
+    <v-toolbar-side-icon class='purple--text' @click='drawer = !drawer'></v-toolbar-side-icon>
       <v-toolbar-title class='text-uppercase grey--text'>
         <span class='font-weight-light'>Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer />
-      <v-btn flat color='grey'>
+      <v-btn text color='grey'>
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-app-bar>
+    <v-navigation-drawer app v-model='drawer' class='indigo'>
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      drawer: true
+    }
+  }
 }
 </script>
