c600adebc4d36add3a7436646d5e25d6e7d59aa6
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 14:00:20 -0400

    part 6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,7 @@
 <template>
   <div class='home'>
     <h1>Home page</h1>
-    <v-btn class='pink white--text'>Click Me</v-btn>
-    <v-btn depressed color='pink' >Click mee</v-btn>
-    <v-btn flat color='pink'>Click</v-btn>
-    <v-btn depressed class='pink white--text'>
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-    <v-btn depressed small class='pink white--text'>
-      <v-icon small left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-    <v-btn large depressed class='pink white--text'>
-      <v-icon small right>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-    <v-btn fab depressed small dark color='purple'>
-      <v-icon>favorite</v-icon>
-    </v-btn>
+    <v-btn class='hidden-sm-only'>Click me</v-btn>
   </div>
 </template>
 
