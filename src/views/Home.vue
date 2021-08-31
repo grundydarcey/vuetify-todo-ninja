bde855d6ada31ec259db3b22b230cc4498492a3f
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 13:29:21 -0400

    part 4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class='home'>
     <h1>Home page</h1>
+    <p class='red white--text'>lorem  ipsum</p>
+    <p class='pink lighten-4 red--text text--darken-4'>loreino ipsumo</p>
+    <h1 class='display-4'>Massive Display</h1>
+    <h4 class='display-1'>Small dis</h4>
+    <p class='headline'>Headline</p>
+    <p class='subheading font-weight-bold'>Subheading here</p>
+    <p class='caption'>Caption here</p>
   </div>
 </template>
 
