fae9004633551fea1ae3ea0b5c302d242439d4fb
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-01 11:22:19 -0400

    part 22

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -7,11 +7,25 @@
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer />
+
+      <v-menu offset-y>
+        <v-btn slot='activator' color='grey'>
+          <v-icon left>expand_more</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-item v-for='link in links' :key='link.text' router :to='link.route'>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>            
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text color='grey'>
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+    
     <v-navigation-drawer app v-model='drawer' class='primary'>
       <v-layout column align-center>
         <v-flex class='mt-5'>
