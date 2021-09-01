6bd8c1928fbb2dfbe3925f3f9926d0a8632625e4
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-31 20:28:29 -0400

    part 18

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,16 +2,24 @@
   <div class='dashboard'>
     <h1 class='subheading grey--text'>Dashboard</h1>
     <v-container class='my-5'>
-  
-      <v-layout row class='mb-3'>
-        <v-btn small flat color='grey' @click='sortBy("title")'>
-          <v-icon left small>folder</v-icon>
-          <span class='caption text-lowercase'>By project name</span>
-        </v-btn>
-        <v-btn small flat color='grey' @click='sortBy("person")'>
-          <v-icon left small>person</v-icon>
-          <span class='caption text-lowercase'>By person</span>
-        </v-btn>
+
+      <v-layout row justify-start class='mb-3'>
+        <v-tooltip top>
+          <v-btn small color='grey' @click='sortBy("title")' slot='activator'>
+            <v-icon left small>folder</v-icon>
+            <span class='caption text-lowercase'>By project name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+       
+       <v-tooltip top>
+          <v-btn small color='grey' @click='sortBy("person")' slot='activator'>
+            <v-icon left small>person</v-icon>
+            <span class='caption text-lowercase'>By person</span>
+          </v-btn>
+          <span>Sort by project</span>
+       </v-tooltip>
+        
       </v-layout>
 
       <v-card flat class='pa-3' v-for='project in projects' :key='project.title'>
