05a2c72deaaf9d2680fdd29d5239770be06bdb6d
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-01 16:18:07 -0400

    part 24

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -17,7 +17,7 @@
         </template>
         <v-list>
           <v-list-item v-for='link in links' :key='link.text' router :to='link.route'>
-            <v-list-item-title>{{ link.title }}</v-list-item-title>            
+            <v-list-item-title>{{ link.text }}</v-list-item-title>            
           </v-list-item>
         </v-list>
       </v-menu>
--- src/components/popup.vue
+++ src/components/popup.vue
@@ -9,11 +9,29 @@
       <v-card-title>
         <h2>Add a New Project</h2>
       </v-card-title>
+      <v-card-text>
+        <v-form class='px-3'>
+          <v-text-field label='Title' prepend-icon='folder' v-model='title'></v-text-field>
+          <v-textarea label='information' v-model='content' prepend-icon='edit'></v-textarea>
+          <v-btn class='success mx-0 mt-3' @click='submit'>Add project</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content)
+    }
+  }
 }
 </script>
