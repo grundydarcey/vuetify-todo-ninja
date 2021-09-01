1981ddba85cc1cd7046b1693239acdbb6422090d
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-01 11:31:37 -0400

    part 23

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -34,6 +34,9 @@
           </v-avatar>
           <p class='white--text subheading mt-1'>Darcey</p>
         </v-flex>
+        <v-flex class='mt-4 mb-3'>
+          <Popup />
+        </v-flex>
       </v-layout>
       <v-list>
         <v-list-item v-for='link in links' :key='link.text' router :to='link.route'>
@@ -50,7 +53,9 @@
 </template>
 
 <script>
+import Popup from './popup';
 export default {
+  components: { Popup },
   data() {
     return {
       drawer: false,
